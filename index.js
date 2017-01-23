02c2f5d2da5a978362a641ccb8afe495dd4f14e0
Eric <erjohnson.pdx@gmail.com>
2017-01-22 16:54:38 -0800

    Add #lint function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const textract = require('textract');
 const validUrl = require('valid-url');
 const terms = JSON.parse(fs.readFileSync(__dirname + "/terms.json", "utf8"))['all'];
 
@@ -10,6 +11,31 @@ function _isUrl(input) {
   }
 }
 
+function lint(input) {
+  let text;
+  let matches = [];
+
+  switch (_isUrl(input)) {
+    case true:
+      textract.fromUrl(url, function( error, res ) {
+        text = res.toLowerCase()
+      })
+      break;
+    case false:
+      text = input.toLowerCase()
+      break;
+  }
+
+  for (const term of terms) {
+    if (text.indexOf(term) > -1) {
+      matches.push(term);
+    };
+  };
+
+  return matches;
+}
+
 module.exports = {
-  _isUrl: _isUrl
+  _isUrl: _isUrl,
+  lint: lint
 }
--- test.js
+++ test.js
@@ -6,13 +6,42 @@ describe('loaded-language-linter', function() {
     it('should return true when given a valid url.', function() {
       let testUrl = 'https://www.google.com/'
       let result = linter._isUrl(testUrl)
-      assert.equal(result, true)
+      assert.equal(true, result)
     })
 
     it('should return false when given a string.', function() {
       let testString = 'Here is a string'
       let result = linter._isUrl(testString)
-      assert.equal(result, false)
+      assert.equal(false, result)
     })
+  })
+
+  describe('#lint', function() {
+    it('should return 0 matches when no terms are present.', function() {
+      let testString = 'Lorem ipsum dolor sit amet'
+      let result = linter.lint(testString)
+      assert.equal(0, result.length)
+    })
+
+    it('should ignore capitalization.', function() {
+      let testString = 'That person is a Socialist.';
+      let result = linter.lint(testString);
+      assert.equal("socialist", result[0])
+      assert.equal(1, result.length);
+    });
+
+    it('should work with multi word phrases', function() {
+      let testString = 'They are a member of the ruling class.'
+      let result = linter.lint(testString);
+      assert.equal('ruling class', result[0])
+      assert.equal(1, result.length);
+    });
+
+    it('should not have duplicate matches', function() {
+      let testString = 'The ruling class. The ruling class'
+      let result = linter.lint(testString);
+      assert.equal('ruling class', result[0])
+      assert.equal(1, result.length);
+    });
   })
 })
