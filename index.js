1e2cbb01c2741758ea6631bb110cd7c017e75053
Eric <erjohnson.pdx@gmail.com>
2017-01-22 16:32:24 -0800

    Add #_getType

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const fs = require('fs');
+const url = require('url');
+const path = require('path');
+const validUrl = require('valid-url');
+const validFile = require('valid-file');
+const terms = JSON.parse(fs.readFileSync(__dirname + "/terms.json", "utf8"))['all'];
 
+function _getType(input) {
+  if (validUrl.isUri(input)) {
+    return 0
+  } else if (validFile.sync(input)) {
+    return 1
+  } else {
+    return 2
+  }
+}
+
+module.exports = {
+  _getType: _getType
+}
--- test.js
+++ test.js
@@ -1,9 +1,24 @@
 const assert = require('assert');
+const linter = require(__dirname + "/index.js");
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
+describe('loaded-language-linter', function() {
+  describe('#_getType', function() {
+    it('should return 0 when given a valid url.', function() {
+      let testUrl = 'https://www.google.com/'
+      let result = linter._getType(testUrl)
+      assert.equal(result, 0)
+    })
+
+    it('should return 1 when given a valid file.', function() {
+      let testFile = __dirname + "/test.js"
+      let result = linter._getType(testFile)
+      assert.equal(result, 1)
+    })
+
+    it('should return 2 when given a string.', function() {
+      let testString = 'Here is a string'
+      let result = linter._getType(testString)
+      assert.equal(result, 2)
+    })
+  })
+})
