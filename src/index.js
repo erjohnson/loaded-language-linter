4c81a9f66703605fafb58b04fccb490a29503b79
Eric <erjohnson.pdx@gmail.com>
2017-01-23 17:58:10 -0800

    Remove pulling text from url, remove dependencies

--- src/index.js
+++ src/index.js
@@ -1,27 +1,15 @@
-import fs from 'fs';
-import textract from 'textract';
-import validUrl from 'valid-url';
+import fs from 'fs'
 
 module.exports = function (input) {
 
   const terms = JSON.parse(fs.readFileSync(__dirname + "/../terms.json", "utf8"))['all'];
 
-  let text;
+  let text = input;
+  let lowerCaseText = input.toLowerCase();
   let matches = [];
 
-  if (validUrl.isUri(input)) {
-    textract.fromUrl(input, {preserveLineBreaks: true}, function (error, res) {
-      if (error) { console.log(error) }
-      text = res;
-    })
-  } else {
-    text = input;
-  }
-
-  let lcText = text.toLowerCase();
-
   for (const term of terms) {
-    if (lcText.indexOf(term) > -1) {
+    if (lowerCaseText.indexOf(term) > -1) {
       matches.push(term);
     }
   }
